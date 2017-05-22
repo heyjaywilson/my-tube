ee45a59e1f001eb4bb5a751c8572f1af8ed84212
maeganjwilson <maegan@maeganwilson.com>
2017-05-22 12:38:05 -0500

    Grabs item from search bar

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@ var template = '<li><img><div><h4><p></p></h4></div></li>';
 const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 function addSearchResult(){
-    
+
 }
+
+function watchSubmit(){
+    $('#search').submit(function(e){
+        e.preventDefault();
+        var searchTerm = $(this).find('#item').val();
+        console.log(searchTerm);
+    });
+}
+$(function(){watchSubmit();});
